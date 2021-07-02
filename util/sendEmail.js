35375b5b288345af74a7fce3016263b798172022
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-02 13:04:26 +0100

    vfs

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -10,7 +10,7 @@ const sendEmail = async (email, subject, text) => {
             secure: true,
             auth: {
                 user: 'laelbaker@gmail.com',
-                pass: '4rtghlae',
+                pass: 'vphztuqtydedfvzw',
             },
         });
 
