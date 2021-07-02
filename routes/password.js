576926aa9911422c99e4b48d3e7435e0645ee2ce
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-02 12:30:26 +0100

    vfs

--- routes/password.js
+++ routes/password.js
@@ -1,6 +1,19 @@
 const  path = require('path');
 const express = require('express');
+const Users = require('../models/users');
 const router = express.Router();
+const sendEmail = require("../utils/email");
 
+router.post('/reset', async(req, res, next) => {
+    const {id} = req.body;
+    Users.find({_id: id}, async (err, result) => {
+        if (!err) {
+            let email = 'laelbaker@hotmail.co.uk'
+            let link = result[0].password;
+            await sendEmail(email, 'Subject', link)
+            res.send('response')
+        }
+    })
+})
 
 exports.routes = router;
