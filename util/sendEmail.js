b006557f831e3665ded72e22ed0a33c83df7555a
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-02 14:35:38 +0100

    vfs

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -2,9 +2,6 @@ const nodemailer = require("nodemailer");
 var nodeoutlook = require('nodejs-nodemailer-outlook')
 
 const sendEmail = async (email, subject, text) => {
-    try {
-        console.log(email, subject, text)
-        //let testAccount = await nodemailer.createTestAccount();
         return nodeoutlook.sendEmail({
             auth: {
                 user: 'laelbaker@hotmail.co.uk',
@@ -27,10 +24,7 @@ const sendEmail = async (email, subject, text) => {
         //     text: text,
         // });
 
-        console.log("email sent sucessfully");
-    } catch (error) {
-        console.log(error, "email not sent");
-    }
+      
 };
 
 module.exports = sendEmail;
