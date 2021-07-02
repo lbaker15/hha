4caf29bd747b1c03010e360dfe98c657cabe99ac
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-02 13:57:40 +0100

    vfs

--- routes/password.js
+++ routes/password.js
@@ -8,8 +8,9 @@ router.post('/reset', async(req, res, next) => {
     const {id} = req.body;
     Users.find({_id: id}, async (err, result) => {
         if (!err) {
-            let email = 'laelbaker@hotmail.co.uk'
+            let email = 'lbaker@bws.digital'
             let link = result[0].password;
+            console.log('LINK', link)
             await sendEmail(email, 'Subject', link)
             res.send('response')
         }
