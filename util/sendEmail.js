1b2ccff0b4ba6bcd5ab21fcbcdfa23e3a1edd537
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-02 14:03:48 +0100

    vsd

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -1,26 +1,31 @@
 const nodemailer = require("nodemailer");
+var nodeoutlook = require('nodejs-nodemailer-outlook')
 
 const sendEmail = async (email, subject, text) => {
     try {
         //let testAccount = await nodemailer.createTestAccount();
-        const transporter = nodemailer.createTransport({
+        const transporter = nodeoutlook.sendMail({
             service: 'hotmail',
             auth: {
                 user: 'laelbaker@hotmail.co.uk',
                 password: '4rtghlae'
-            }
+            },
+            from: 'laelbaker@hotmail.co.uk',
+            to: email,
+            subject: subject,
+            text: text
             // auth: {
             //     user: 'laelbaker@gmail.com',
             //     pass: 'vphztuqtydedfvzw',
             // },
         });
 
-        await transporter.sendMail({
-            from: 'laelbaker@hotmail.co.uk',
-            to: email,
-            subject: subject,
-            text: text,
-        });
+        // await transporter.sendMail({
+        //     from: 'laelbaker@hotmail.co.uk',
+        //     to: email,
+        //     subject: subject,
+        //     text: text,
+        // });
 
         console.log("email sent sucessfully");
     } catch (error) {
