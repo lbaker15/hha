379a3a5556f4ddae5d653888b6095d0f9cd6d991
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-02 13:33:45 +0100

    vfs

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -3,10 +3,12 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, text) => {
     try {
         //let testAccount = await nodemailer.createTestAccount();
-        const transporter = nodemailer.createTransport('SMTP',{
-            service: 'hotmail', // hostname
+        const transporter = nodemailer.createTransport({
+            host: "smtp.123-reg.co.uk", // hostname
+            secureConnection: true, // TLS requires secureConnection to be false
+            port: 587, // port for secure SMTP
             auth: {
-                user: 'laelbaker@hotmail.co.uk',
+                user: 'lbaker@bws.digital',
                 password: '4rtghlae'
             }
             // auth: {
@@ -16,7 +18,7 @@ const sendEmail = async (email, subject, text) => {
         });
 
         await transporter.sendMail({
-            from: 'laelbaker@hotmail.co.uk',
+            from: 'lbaker@bws.digital',
             to: email,
             subject: subject,
             text: text,
