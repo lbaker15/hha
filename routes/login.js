369369adfcc96123e740eded86f50f9a89b67feb
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-07 09:17:01 +0100

    vfs

--- routes/login.js
+++ routes/login.js
@@ -14,7 +14,7 @@ const Employee = require('../models/employee');
 router.post('/signup', async (req, res, next) => {
     let {username, password, admin, hcProvider} = req.body;
     bcrypt.hash(password, saltRounds, function(err, hash) {
-        let obj = {name, password: hash, admin, hcProvider}
+        let obj = {username, password: hash, admin, hcProvider}
         Users.create(obj).then(result => {
             if (result) {
                 res.json({'Success': 'User created'})
