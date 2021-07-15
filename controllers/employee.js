e7fa121509f6305cdf1de3bd898f901d7cc165c5
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-15 11:39:58 +0100

    vfs

--- controllers/employee.js
+++ controllers/employee.js
@@ -9,7 +9,7 @@ var jwt = require('jsonwebtoken');
 const middleware = require('../middleware/auth');
 const Providers = require('../models/providers');
 const Employee = require('../models/employee');
-const HttpError = require('../models/http-error');
+const 2wqrror = require('../models/http-error');
 const helpers = require('./helpers/file');
 
 const deleteFunc = async (req, res, next) => {
@@ -93,7 +93,49 @@ const edit = async (req, res, next) => {
     })
 }
 
+const editProfile = async (req, res, next) => {
+    const {firstname, lastname, id, password, discipline, email, businessAddress} = req.body;
+    let obj = {firstname, lastname, discipline: discipline, email, businessAddress}
+    jwt.verify(req.token, 'secret', function(err, decoded) {
+        if (!err) {
+            Employee.updateOne({userId: id}, {$set: obj}, 
+                (err, result) => {
+                    Employee.find({userId: id}, (err, result) => {
+                        if (result.length > 0) {
+                            let userId = id;
+                            bcrypt.hash(password, saltRounds, function(err, hash) {
+                                Users.updateOne({_id: userId}, {password: hash}, (err, result) => {
+                                    if (!err) {
+                                        res.json({'Success': 'user changed'})
+                                    }
+                                })
+                            })
+                        } else {
+                            res.json({'Failure': 'user not changed'})
+                        }
+                    })
+            })
+        } else {
+            res.json({'Failure': err})
+        }
+    })
+}
+
+const add = async (req, res, next) => {
+    let {
+        firstname, 
+        lastname, discipline, userId,
+        businessAddress, email, author
+    } = req.body;
+    let obj = {userId, lastname: String(lastname).toLowerCase(), firstname: String(firstname).toLowerCase(), discipline, businessAddress: String(businessAddress).toLowerCase(), email, author}
+    let add = new Employee(obj)
+    await add.save().then(data => {
+        return res.json({'Data': data})
+    })
+}
 
 exports.deleteFunc = deleteFunc;
 exports.employeeList = employeeList;
 exports.edit = edit;
+exports.editProfile = editProfile;
+exports.add = add;
--- routes/employee.js
+++ routes/employee.js
@@ -17,46 +17,8 @@ router.post('/employee-list', middleware.verifyToken, controller.employeeList)
 
 router.post('/edit-employee', middleware.verifyToken, controller.edit)
 
-router.post('/edit-employee-profile', middleware.verifyToken, async (req, res, next) => {
-    const {firstname, lastname, id, password, discipline, email, businessAddress} = req.body;
-    let obj = {firstname, lastname, discipline: discipline, email, businessAddress}
-    jwt.verify(req.token, 'secret', function(err, decoded) {
-        if (!err) {
-            Employee.updateOne({userId: id}, {$set: obj}, 
-                (err, result) => {
-                    Employee.find({userId: id}, (err, result) => {
-                        if (result.length > 0) {
-                            let userId = id;
-                            bcrypt.hash(password, saltRounds, function(err, hash) {
-                                Users.updateOne({_id: userId}, {password: hash}, (err, result) => {
-                                    if (!err) {
-                                        res.json({'Success': 'user changed'})
-                                    }
-                                })
-                            })
-                        } else {
-                            res.json({'Failure': 'user not changed'})
-                        }
-                    })
-            })
-        } else {
-            res.json({'Failure': err})
-        }
-    })
-})
+router.post('/edit-employee-profile', middleware.verifyToken, controller.editProfile)
 
-
-router.post('/add-employee', async (req, res, next) => {
-    let {
-        firstname, 
-        lastname, discipline, userId,
-        businessAddress, email, author
-    } = req.body;
-    let obj = {userId, lastname: String(lastname).toLowerCase(), firstname: String(firstname).toLowerCase(), discipline, businessAddress: String(businessAddress).toLowerCase(), email, author}
-    let add = new Employee(obj)
-    await add.save().then(data => {
-        return res.json({'Data': data})
-    })
-})
+router.post('/add-employee', controller.add)
 
 exports.routes = router;
